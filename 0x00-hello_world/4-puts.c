50483a9b89cb8531a2c1088545923423809a0418
ISRAARashid <https://github.com/ISRAARashid>
2023-03-16 18:22:04 -0700

    hggjg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
-
-
-
 /**
- * main - printf "Programming is like building a multilingual
- * puzzle, followed by  a newline."
+ * main - Entry point
+ * 
  * Return: always 0  (Success)
  */
-
 int main(void)
-
-
 {
-    puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-    return(0);
+return(0);
 }
