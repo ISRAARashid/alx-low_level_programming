882d6e435b9319a3bb8285ff9f91fb24d1e1d254
ISRAARashid <https://github.com/ISRAARashid>
2023-03-16 18:41:26 -0700

    uuru

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - Entry point
-* 
-* Return: always 0  (Success)
+* main - prints "Programming is like building a multilingual
+* puzzle," followed by a new line.
+* Return: always 0.
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
