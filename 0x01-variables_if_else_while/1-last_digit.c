74adaba7344c6441c977ba76a44163be758c8808
ISRAARashid <https://github.com/ISRAARashid>
2023-03-17 18:46:05 -0700

    Last Digit Check print new edits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
+#include <stdio.h>
 
 /* betty style doc for function main goes there */
 /* THis Function is for Getting the Last digit Check */
