f61b715c3b4733a1968ec0a6f2f5c44887191b74
ISRAARashid <https://github.com/ISRAARashid>
2023-03-17 18:40:29 -0700

    Last Digit Check print new edits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,6 +3,7 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/* THis Function is for Getting the Last digit Check */
 int main(void)
 {
 	int n;
@@ -10,11 +11,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if((n%10)>5)
-		pritf("Last digit of %d is %d and is greater than 5\n", n ,(n%10));
-	else if((n%10)==0)
-		pritf("Last digit of %d is %d and is 0\n", n ,(n%10));
+	if ((n % 10) > 5)
+		pritf("Last digit of %d is %d and is greater than 5 \n", n, (n % 10));
+	else if ((n % 10) == 0)
+		pritf("Last digit of %d is %d and is 0 \n", n, (n % 10));
 	else
-		pritf("Last digit of %d is %d and is less than 6 and not 0\n", n ,(n%10));
+		pritf("Last digit of %d is %d and is less than 6 and not 0 \n", n, (n % 10));
 	return (0);
 }
