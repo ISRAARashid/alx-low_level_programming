ec463462ef5f95cf77eaff5a9853f474855adb60
ISRAARashid <https://github.com/ISRAARashid>
2023-03-17 19:50:52 -0700

    all numbers with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main  -     print the alphabits.
+ * Return: 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(40);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
