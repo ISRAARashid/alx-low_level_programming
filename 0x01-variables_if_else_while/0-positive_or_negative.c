43bfb781f28232398c3e11df0a9ad157198cffdf
ISRAARashid <https://github.com/ISRAARashid>
2023-03-17 17:56:28 -0700

    trtr

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,13 +3,14 @@
 #include <stdio.h>
 
 /**
- * main - print a random num
+ * main	 -	print a random num
  *
  * Return: 0.
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
