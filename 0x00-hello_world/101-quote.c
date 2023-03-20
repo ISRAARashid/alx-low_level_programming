2b2c5d949830e2ae22f9ad2e178a5e8d384321be
ISRAARashid <https://github.com/ISRAARashid>
2023-03-20 16:59:54 -0700

    trtrt

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
-#include <unistd.h>
+#include <unistd>
 
 /**
- * main - "prints exactly and that piece of art is useful" - Dora Korpar,  2015-10-19",
- *followed by a new line, to the standard error.
+ * main	-	prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a newline, to standard error.
  * Return: Always 1.
  */
+
+
 int main(void)
+
 {
-        write(2, "and that piece of art is useful"\ - Dora Korpar,  2015-10-19\n", 59);
-       	return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
