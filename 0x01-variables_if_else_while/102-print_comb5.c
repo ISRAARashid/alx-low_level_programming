305616514ef8fc013b04c666d480d03904fa5b8d
ISRAARashid <https://github.com/ISRAARashid>
2023-03-20 17:47:52 -0700

    trtrt

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main  -     print the alphabits.
+ * Return: 0.
+ */
+int main(void)
+{
+	int i;
+	int x;
+	int k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (x = '0'; x <= '9'; x++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if ((i < x) && (x < k))
+				{
+					putchar(i);
+					putchar(x);
+					putchar(k);
+					if ((i == '7') && (x == '8') && (k == '9'))
+					{}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
