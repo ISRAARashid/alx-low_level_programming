79e728835c2c3613324e05f231d5d68b78c261b7
ISRAARashid <https://github.com/ISRAARashid>
2023-03-23 16:49:30 -0700

    trttr

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,31 +1,34 @@
 #include "main.h"
+
 /**
- * print_triangle - print a triangle of squares according paranetes
- *@size: the size of squares triangles
- * Return: empty
+ * print_triangle -  checks for checks for a digit (0 through 9).
+ * @size: size -  Variable
+ * Return: Always 0.
  */
 void print_triangle(int size)
-
 {
-	int x, y, z;
+	int x, y;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-	_putchar('/n');
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y <= size; y++)
+			{
+				if (y <= size - x)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
 	}
 	else
 	{
-	for (x = 0; x < size; x++)
-	{
-	for (y = size - x; y > 1; y--)
-	{
-	_putchar(32);
-	}
-	for (z = 0; z <= x; z++)
-	{
-	_putchar(35);
-	}
-	_putchar('/n');
-	}
+		_putchar('\n');
 	}
 }
