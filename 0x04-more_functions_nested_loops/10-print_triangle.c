0cd9c18cb6059004f46a292c61ae016056390894
ISRAARashid <https://github.com/ISRAARashid>
2023-03-23 15:43:01 -0700

    trttr

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,21 +5,23 @@
  * Return: empty
  */
 void print_triangle(int size)
+
 {
 	int x, y, z;
+
 	if (size <= 0)
 	{
 	_putchar('/n');
 	}
 	else
 	{
-	for (x = 0;x < size; x++)
+	for (x = 0; x < size; x++)
 	{
 	for (y = size - x; y > 1; y--)
 	{
 	_putchar(32);
 	}
-	for (z = 0;z <= x; z++)
+	for (z = 0; z <= x; z++)
 	{
 	_putchar(35);
 	}
